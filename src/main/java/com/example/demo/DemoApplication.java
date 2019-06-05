5fd0c79b23e70c431ccc2dcc4a863af1c57eda2a
Vi Nguyen Van <nvvi@tma.com.vn>
2019-06-05 17:36:31 +0700

    first commit

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,28 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+@RequestMapping("/")
+public class DemoApplication {
+
+	@GetMapping("/hello")
+	public String world(){
+		return "world";
+	}
+
+	@GetMapping("/xin")
+	public String chao(){
+		return "chao";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
